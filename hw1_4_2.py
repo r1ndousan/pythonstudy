59e2782c0e211b08215a4f98e5b9f082e1442541
r1ndousan <160651068+r1ndousan@users.noreply.github.com>
2024-05-05 16:29:55 +0300

    Delete hw1_4_2.py

--- hw1_4_2.py
+++
@@ -1,6 +0,0 @@
-# через format
-name = input("Введите имя: ")
-surname = input("Введите фамилию: ")
-age = input("Введите возраст: ")
-
-print("Ваше имя: {}, Фамилия: {}, Возраст: {} лет.".format(name, surname, age))
